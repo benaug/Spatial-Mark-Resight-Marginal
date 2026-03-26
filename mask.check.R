e19e69d0dcbc53fd63ecb4bea5d5f0dfa8abbbcd
Ben Augustine <ba378@cornell.edu>
2026-03-26 10:07:56 -0400

    fixed bug in mask.check so that it works with continuous values now. Added dSS ordering check.

--- mask.check.R
+++ mask.check.R
@@ -5,8 +5,12 @@ mask.check <- function(dSS=NA,cells=NA,n.cells=NA,n.cells.x=NA,n.cells.y=NA,res=
   if(ncol(dSS)!=2)stop("'dSS' must have 2 columns")
   if(dim(cells)[1]!=n.cells.x)stop("'cells' should be of length 'n.cells.x'")
   if(dim(cells)[2]!=n.cells.y)stop("'cells' should be of length 'n.cells.y'")
-  if(!all(range(x.vals)+c(-res/2,res/2)==xlim))stop("x.vals doesn't match up with xlim")
-  if(!all(range(y.vals)+c(-res/2,res/2)==ylim))stop("y.vals doesn't match up with ylim")
+  if(!all(abs(range(x.vals) + c(-res/2, res/2) - xlim) < 1e-9))stop("x.vals doesn't match up with xlim")
+  if(!all(abs(range(y.vals) + c(-res/2, res/2) - ylim) < 1e-9))stop("y.vals doesn't match up with ylim")
+  expected.dSS <- as.matrix(expand.grid(x.vals, y.vals))
+  if(!all(abs(dSS - expected.dSS) < 1e-9)){
+    stop("'dSS' rows are not in the correct order: x must cycle through all values before y increments to the next value")
+  }
   
   for(i in 1:n.cells){
     s.cell.x <- i%%n.cells.x 
